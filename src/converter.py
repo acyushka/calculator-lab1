99a658ae734fe9b22a73c7ea3622a5b724c8ef1c
acyushka <izotovarsenii3005@gmail.com>
2025-10-09 10:41:39 +0300

    fix unary vs pow precedence

--- src/converter.py
+++ src/converter.py
@@ -9,34 +9,42 @@ def convert_to_rpn(tokens: list[Token]) -> list[Token]:
     :return: список токенов в порядке RPN
     """
     stack = []
-    output = []
+    output: list[Token] = []
     for token in tokens:
         token_type, value = token
 
         if token_type == "NUM":
             output.append(token)
         elif token_type == "OP":
+
+            # костыль для mypy
             if isinstance(value, str):
                 value_op: str = value
             op_token = (token_type, value_op)
+
             if value_op == "(":
                 stack.append(op_token)
+
             elif value_op in ("~", "$"):
                 stack.append(op_token)
+
             elif value_op == "**":  # т.к. право-ассоц. оператор: в условии строго <
                 while (stack and stack[-1][-1] != '(' and
                         OPERATOR_PRECEDENCE[value_op] < OPERATOR_PRECEDENCE[stack[-1][-1]]):
                     output.append(stack.pop())
                 stack.append(op_token)
+
             elif value_op in OPERATOR_PRECEDENCE:
                 while (stack and stack[-1][-1] != '(' and
                        OPERATOR_PRECEDENCE[value_op] <= OPERATOR_PRECEDENCE[stack[-1][-1]]):
                     output.append(stack.pop())
                 stack.append(op_token)
+
             elif value_op == ")":
                 while stack and stack[-1][-1] != "(":
                     output.append(stack.pop())
                 stack.pop()
+
         elif token_type == "EOF":
             break
 
